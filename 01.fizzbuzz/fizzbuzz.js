536b808e98f2c0ff9fb88be7340d6974209df7c1
a-kuroki-gs <a.kuroki@genbasupport.com>
2023-11-27 18:39:35 +0900

    FizzBuzzプログラム(JavaScript版)を作成しました

---
+++ 01.fizzbuzz/fizzbuzz.js
@@ -0,0 +1,13 @@
+function fizzbuzz(x){
+  if (x % 15 === 0)
+    console.log('FizzBuzz');
+  else if (x % 5 === 0)
+    console.log('Buzz');
+  else if (x % 3 === 0)
+    console.log('Fizz');
+  else
+    console.log(x);
+}
+
+for (let x = 1; x <= 20; x++)
+  fizzbuzz(x);
