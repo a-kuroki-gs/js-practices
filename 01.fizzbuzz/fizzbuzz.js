01db6b30bc3678a8e465953b2de9549d43fe962f
a-kuroki-gs <a.kuroki@genbasupport.com>
2023-11-27 18:45:33 +0900

    Prettierでフォーマットを修正しました

--- 01.fizzbuzz/fizzbuzz.js
+++ 01.fizzbuzz/fizzbuzz.js
@@ -1,13 +1,8 @@
-function fizzbuzz(x){
-  if (x % 15 === 0)
-    console.log('FizzBuzz');
-  else if (x % 5 === 0)
-    console.log('Buzz');
-  else if (x % 3 === 0)
-    console.log('Fizz');
-  else
-    console.log(x);
+function fizzbuzz(x) {
+  if (x % 15 === 0) console.log("FizzBuzz");
+  else if (x % 5 === 0) console.log("Buzz");
+  else if (x % 3 === 0) console.log("Fizz");
+  else console.log(x);
 }
 
-for (let x = 1; x <= 20; x++)
-  fizzbuzz(x);
+for (let x = 1; x <= 20; x++) fizzbuzz(x);
