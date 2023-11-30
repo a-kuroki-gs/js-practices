e34fc71df64f1a1c4e388e3c9c301aed6a2c3f5e
a-kuroki-gs <a.kuroki@genbasupport.com>
2023-11-30 20:00:53 +0900

    不要なコメントを消去

--- 02.calendar/calender_js.js
+++ 02.calendar/calender_js.js
@@ -1,4 +1,3 @@
-/* 入力値を受け取る */
 var argv = require('minimist')(process.argv.slice(2));
 var year = argv.y;
 var month = argv.m;
@@ -11,17 +10,14 @@ if (typeof year === 'undefined')
 if (typeof month === 'undefined')
   month = today.getMonth() + 1;
 
-/* 始まりの日と終わりの日を取得する */
 var first_date = new Date(Date.UTC(year, month -1, 1))
 var last_date = new Date(Date.UTC(year, month, 0))
 var days = last_date.getDate()
 
-/* m月 y と表示する */
 console.log(`      ${month}月 ${year}`);
 console.log('日 月 火 水 木 金 土');
 process.stdout.write(String(' '.repeat(first_date.getDay() * 3)));
 
-/* 土曜日で改行して表示する */
 for (var date = first_date; date.getDate() <= days; ){
   process.stdout.write(String(date.getDate()).padStart(2) + ' ');
   if (date.getDay() == 6)
