84613065297e300d45544b9af32e8306e2dbeb2f
a-kuroki-gs <a.kuroki@genbasupport.com>
2023-11-30 16:29:30 +0900

    年・月を設定し、始まりの日と終わりの日を取得する

--- 02.calendar/calender_js.js
+++ 02.calendar/calender_js.js
@@ -2,3 +2,15 @@
 var argv = require('minimist')(process.argv.slice(2));
 var year = argv.y;
 var month = argv.m;
+
+var today = new Date();
+
+if (typeof year === 'undefined')
+  year = today.getFullYear();
+
+if (typeof month === 'undefined')
+  month = today.getMonth() + 1;
+
+/* 始まりの日と終わりの日を取得する */
+var first_date = new Date(Date.UTC(year, month -1, 1))
+var last_date = new Date(Date.UTC(year, month, 0))
