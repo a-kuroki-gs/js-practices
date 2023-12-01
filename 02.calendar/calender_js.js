9bd122643818486165a4d471f0f911a40585bf41
a-kuroki-gs <a.kuroki@genbasupport.com>
2023-12-01 09:51:03 +0900

    ESLintとPrettierのチェックを通す

--- 02.calendar/calender_js.js
+++ 02.calendar/calender_js.js
@@ -1,29 +1,32 @@
-var argv = require('minimist')(process.argv.slice(2));
+var argv = require("minimist")(process.argv.slice(2));
 var year = argv.y;
 var month = argv.m;
 
 var today = new Date();
 
-if (typeof year === 'undefined')
+if (typeof year === "undefined") {
   year = today.getFullYear();
+}
 
-if (typeof month === 'undefined')
+if (typeof month === "undefined") {
   month = today.getMonth() + 1;
+}
 
-var first_date = new Date(Date.UTC(year, month -1, 1))
-var last_date = new Date(Date.UTC(year, month, 0))
-var days = last_date.getDate()
+var first_date = new Date(Date.UTC(year, month - 1, 1));
+var last_date = new Date(Date.UTC(year, month, 0));
+var days = last_date.getDate();
 
 console.log(`      ${month}月 ${year}`);
-console.log('日 月 火 水 木 金 土');
-process.stdout.write(String(' '.repeat(first_date.getDay() * 3)));
+console.log("日 月 火 水 木 金 土");
+process.stdout.write(String(" ".repeat(first_date.getDay() * 3)));
 
-for (var date = first_date; date.getDate() <= days; ){
-  process.stdout.write(String(date.getDate()).padStart(2) + ' ');
-  if (date.getDay() == 6)
+for (var date = first_date; date.getDate() <= days; ) {
+  process.stdout.write(String(date.getDate()).padStart(2) + " ");
+  if (date.getDay() == 6) {
     console.log();
-  date.setDate(date.getDate() + 1)
-  if (date.getDate() == 1){
+  }
+  date.setDate(date.getDate() + 1);
+  if (date.getDate() == 1) {
     console.log();
     break;
   }
