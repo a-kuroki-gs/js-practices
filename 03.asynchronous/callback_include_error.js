813731725231ce361fe35f3650c547d49c4b82af
a-kuroki-gs <a.kuroki@genbasupport.com>
2023-12-08 11:15:23 +0900

    コールバック形式のコードを作成

---
+++ 03.asynchronous/callback_include_error.js
@@ -0,0 +1,26 @@
+import sqlite3 from 'sqlite3';
+const db = new sqlite3.Database(":memory:")
+
+function test() {
+  db.run("CREATE TABLE books(id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT NOT NULL UNIQUE)", (err) => {
+    if (err) {
+      console.error(err.message);
+    } else {
+      db.run("INSERT INTO books(title) VALUES(?)", [], (err) => {
+        if (err) {
+          console.error(err.message);
+        }
+      });
+      db.all("SELECT books_id FROM books", [], (err, rows) => {
+        if (err) {
+          console.error(err.message);
+        } else {
+          console.log(rows);
+        }
+        db.close();
+      });
+    }
+  });
+}
+
+test()
---
+++ 03.asynchronous/callback_not_error.js
@@ -0,0 +1,34 @@
+import sqlite3 from 'sqlite3';
+const db = new sqlite3.Database(":memory:")
+
+function test() {
+  db.run("CREATE TABLE books(id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT NOT NULL UNIQUE)", (err) => {
+    if (err) {
+      console.error(err.message);
+    } else {
+      db.run("INSERT INTO books(title) VALUES(?)", ["Test"], (err) => {
+        if (err) {
+          console.error(err.message);
+        } else {
+          db.all("SELECT id FROM books", [], (err, rows) => {
+            if (err) {
+              console.error(err.message);
+            } else {
+              console.log(rows);
+              db.all("SELECT * FROM books", [], (err, records) => {
+                if (err) {
+                  console.error(err.message);
+                } else {
+                  console.log(records);
+                  db.close();
+                }
+              });
+            }
+          });
+        }
+      });
+    }
+  });
+}
+
+test()
