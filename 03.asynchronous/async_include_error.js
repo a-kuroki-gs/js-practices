4b052f7f1121bebc56aef68005884b83adf84bdf
a-kuroki-gs <a.kuroki@genbasupport.com>
2023-12-11 10:20:39 +0900

    async/awaitの形式で作成

---
+++ 03.asynchronous/async_include_error.js
@@ -0,0 +1,27 @@
+import sqlite3 from 'sqlite3';
+
+import { create_table, insert_record, select_id_from_table, drop_table } from "./promise_methods.js";
+
+async function test() {
+  const db = new sqlite3.Database(":memory:");
+
+  try {
+    await create_table(db, "books");
+    await insert_record(db, "book", "Test");
+  } catch (e) {
+    if (e instanceof Error && e.errno === 1 && e.code === 'SQLITE_ERROR') {
+      console.log(e.message);
+    }
+  }
+  try {
+    await select_id_from_table(db, "books", "books_id");
+  } catch (e) {
+    if (e instanceof Error && e.errno === 1 && e.code === 'SQLITE_ERROR') {
+      console.log(e.message);
+    }
+  }
+  await drop_table(db, "books");
+  db.close();
+}
+
+test();
---
+++ 03.asynchronous/async_not_error.js
@@ -0,0 +1,18 @@
+import sqlite3 from 'sqlite3';
+
+import { create_table, insert_record, select_id_from_table, select_all_from_table, drop_table } from "./promise_methods.js";
+
+async function test() {
+  const db = new sqlite3.Database(":memory:");
+
+  await create_table(db, "books");
+  await insert_record(db, "books", "Test");
+  const rows = await select_id_from_table(db, "books", "id");
+  console.log(rows);
+  const records = await select_all_from_table(db, "books");
+  console.log(records);
+  await drop_table(db, "books");
+  db.close();
+}
+
+test();
