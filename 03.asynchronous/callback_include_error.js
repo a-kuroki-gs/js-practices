6194627969ba97039bf2beada38d6b6f570e356e
a-kuroki-gs <a.kuroki@genbasupport.com>
2023-12-11 10:37:23 +0900

    コールバックの不要なエラー処理を削除

--- 03.asynchronous/callback_include_error.js
+++ 03.asynchronous/callback_include_error.js
@@ -1,31 +1,22 @@
 import sqlite3 from 'sqlite3';
-const db = new sqlite3.Database(":memory:")
+const db = new sqlite3.Database(":memory:");
 
 function test() {
-  db.run("CREATE TABLE books(id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT NOT NULL UNIQUE)", (err) => {
-    if (err) {
-      console.error(err.message);
-    } else {
-      db.run("INSERT INTO books(title) VALUES(?)", [], (err) => {
+  db.run("CREATE TABLE books(id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT NOT NULL UNIQUE)", () => {
+    db.run("INSERT INTO books(title) VALUES(?)", [], (err) => {
+      if (err) {
+        console.error(err.message);
+      }
+      db.all("SELECT books_id FROM books", [], (err) => {
         if (err) {
           console.error(err.message);
         }
+        db.run("DROP TABLE books", () => {
+          db.close();
+        });
       });
-      db.all("SELECT books_id FROM books", [], (err, rows) => {
-        if (err) {
-          console.error(err.message);
-        } else {
-          console.log(rows);
-        }
-      });
-      db.run("DROP TABLE books", (err) => {
-        if (err) {
-          console.error(err.message);
-        }
-      db.close();
-      });
-    }
+    });
   });
 }
 
-test()
+test();
--- 03.asynchronous/callback_not_error.js
+++ 03.asynchronous/callback_not_error.js
@@ -1,39 +1,20 @@
 import sqlite3 from 'sqlite3';
-const db = new sqlite3.Database(":memory:")
+const db = new sqlite3.Database(":memory:");
 
 function test() {
-  db.run("CREATE TABLE books(id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT NOT NULL UNIQUE)", (err) => {
-    if (err) {
-      console.error(err.message);
-    } else {
-      db.run("INSERT INTO books(title) VALUES(?)", ["Test"], (err) => {
-        if (err) {
-          console.error(err.message);
-        } else {
-          db.all("SELECT id FROM books", [], (err, rows) => {
-            if (err) {
-              console.error(err.message);
-            } else {
-              console.log(rows);
-              db.all("SELECT * FROM books", [], (err, records) => {
-                if (err) {
-                  console.error(err.message);
-                } else {
-                  console.log(records);
-                  db.run("DROP TABLE books", (err) => {
-                    if (err) {
-                      console.error(err.message);
-                    }
-                  db.close();
-                  });
-                }
-              });
-            }
+  db.run("CREATE TABLE books(id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT NOT NULL UNIQUE)", () => {
+    db.run("INSERT INTO books(title) VALUES(?)", ["Test"], () => {
+      db.all("SELECT id FROM books", [], (err, rows) => {
+        console.log(rows);
+        db.all("SELECT * FROM books", [], (err, records) => {
+          console.log(records);
+          db.run("DROP TABLE books", () => {
+            db.close();
           });
-        }
+        });
       });
-    }
+    });
   });
 }
 
-test()
+test();
