fe93db247cf5edf6e0188d8133c0b95798f8b6d5
a-kuroki-gs <a.kuroki@genbasupport.com>
2023-12-12 11:36:45 +0900

    メモの１行目はメソッドで取り出すよう修正

--- 04.class/memo.js
+++ 04.class/memo.js
@@ -1,8 +1,11 @@
 class Memo {
   constructor(content) {
     this.content = content;
-    this.first_line = content.split('\n')[0];
     this.id = 0;
+  }
+
+  first_line() {
+    return this.content.split('\n')[0];
   }
 
   delete() {
