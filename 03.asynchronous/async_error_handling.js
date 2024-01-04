8b184cb925d574efe6c1465c265805c3d6cb54a0
a-kuroki-gs <a.kuroki@genbasupport.com>
2024-01-04 11:19:24 +0900

    エラーの記載をerrで統一する

--- 03.asynchronous/async_error_handling.js
+++ 03.asynchronous/async_error_handling.js
@@ -11,16 +11,16 @@ async function main() {
   );
   try {
     await runDbQuery(db, "INSERT INTO book(title) VALUES(?)", ["Test"]);
-  } catch (e) {
-    if (e instanceof Error && e.code === "SQLITE_ERROR") {
-      console.log(e.message);
+  } catch (err) {
+    if (err instanceof Error && err.code === "SQLITE_ERROR") {
+      console.log(err.message);
     }
   }
   try {
     await getAllFromDb(db, "SELECT books_id FROM books");
-  } catch (e) {
-    if (e instanceof Error && e.code === "SQLITE_ERROR") {
-      console.log(e.message);
+  } catch (err) {
+    if (err instanceof Error && err.code === "SQLITE_ERROR") {
+      console.log(err.message);
     }
   }
   await runDbQuery(db, "DROP TABLE books");
